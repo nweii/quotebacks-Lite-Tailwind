832412b527bbe7505c61671387b7d75ca957e298
Nathan <nthn.wei@gmail.com>
2023-02-09 14:57:41 +0000

    First commit

---
+++ quotebacksLite.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+	// get all our classed blockquote components
+	let index = document.querySelectorAll(".quoteback");
+	
+	for (let item = 0; item < index.length; item++){
+		
+	}
+}
